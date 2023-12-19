3c5c1a09e5d1fd11d7437a1695a404e882d5f9bd
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-19 22:18:41 +0000

    working on fetching categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { getDatabase } from '@/utils/db';
+import { getCategories, getDatabase } from '@/utils/db';
 import Link from 'next/link';
 import { NotionAPI } from 'notion-client';
 
@@ -9,17 +9,31 @@ const databaseId = process.env.NOTION_DB_ID as string;
 
 export default async function Home() {
   const pages = await getDatabase();
+  const categories = await getCategories();
   console.log(Object.keys(pages));
   return (
     <main>
       <h1 className="text-7xl">Hello world!!</h1>
-      {pages.results.map((page) => {
-        return (
-          <p key={page.id}>
-            <Link href={`/pages/${page.id}`}>{page.id}</Link>
-          </p>
-        );
-      })}
+      <div className="grid grid-cols-2 mx-auto">
+        <div className="grid mx-auto">
+          {pages.results.map((page) => {
+            return (
+              <p key={page.id}>
+                <Link href={`/pages/${page.id}`}>{page.id}</Link>
+              </p>
+            );
+          })}
+        </div>
+        <div className="grid mx-auto">
+          {categories.results.map((result) => {
+            return (
+              <p key={result.id}>
+                <Link href={`/pages/${result.id}`}>{result.id}</Link>
+              </p>
+            );
+          })}
+        </div>
+      </div>
     </main>
   );
 }
--- utils/db.ts
+++ utils/db.ts
@@ -2,6 +2,8 @@ import { Client } from '@notionhq/client';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DB_ID as string;
+const categoriesId =
+  '222e4acf63964c7e90c55228768a2db9?v=64300174648c41ee8a84e94f80881a64' as string;
 
 export async function getDatabase() {
   //* Initial query made looking for complete pages/posts */
@@ -17,6 +19,23 @@ export async function getDatabase() {
         },
       ],
     },
+  });
+  return response;
+}
+export async function getCategories() {
+  //* Initial query made looking for complete pages/posts */
+  const response = await notion.pages.retrieve({
+    database_id: categoriesId,
+    // filter: {
+    //   or: [
+    //     {
+    //       property: 'complete',
+    //       checkbox: {
+    //         equals: true,
+    //       },
+    //     },
+    //   ],
+    // },
     //* Can add more filters (or/and) and also a sort function */
     // sorts: [
     //   {
