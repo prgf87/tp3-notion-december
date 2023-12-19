8d3ac79b385ff7fb5b2b07323d1981a85277268c
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-19 23:41:05 +0000

    ssg by default

--- app/pages/[slug]/page.tsx
+++ app/pages/[slug]/page.tsx
@@ -6,43 +6,10 @@ const notion = new NotionAPI();
 
 type Props = { params: { slug: string } };
 
-export async function getStaticPaths() {
-  // Call an external API endpoint to get posts
-  const res = await fetch('https://.../posts');
-  const posts = await res.json();
-
-  // Get the paths we want to pre-render based on posts
-  const paths = posts.map((post) => ({
-    params: { id: post.id },
-  }));
-
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
-  return { paths, fallback: false };
-}
-
-export async function getStaticProps({ params }: Props) {
+export default async function page({ params }: Props) {
   const { slug } = params;
   //   console.log(slug);
   const recordMap = await notion.getPage(slug);
-  // params contains the post `id`.
-  // If the route is like /posts/1, then params.id is 1
-  // const res = await fetch(`https://.../posts/${params.id}`)
-  // const post = await res.json()
-
-  // Pass post data to the page via props
-  return { props: { recordMap } };
-}
-
-// export async function generateStaticParams(pageId: string) {
-//   const posts = await fetch('https://.../posts').then((res) => res.json());
-
-//   return posts.map((post) => ({
-//     slug: post.slug,
-//   }));
-// }
-
-export default async function page({ posts }) {
   return (
     <div>
       <NotionPage recordMap={recordMap} />
