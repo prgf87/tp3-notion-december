d0861c6bf8f70325d7601a013a4975ff1f6631f1
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-19 20:04:35 +0000

    using original notion-client by Travis Fischer

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
-import { getDatabase } from '@/utils/db';
+// import { getDatabase } from '@/utils/db';
+import { NotionAPI } from 'notion-client';
+import { NotionRenderer } from 'react-notion-x';
 
-const fetchData = async () => {
-  const response = await getDatabase().then(() => {
-    console.log('done!');
-  });
+const notion = new NotionAPI();
+const databaseId = process.env.NOTION_DB_ID as string;
+
+// import { Client } from '@notionhq/client';
+
+const getStaticProps = async () => {
+  const db = await notion.getPage(databaseId);
+  console.log(db);
+
+  return db;
 };
 
-export default async function Home() {
-  const homePage = await fetchData();
-
+export default async function Home({ db }) {
   return (
     <main>
       <h1 className="text-7xl">Hello world!!</h1>
+      <NotionRenderer recordMap={db} fullPage={true} darkMode={false} />
     </main>
   );
 }
