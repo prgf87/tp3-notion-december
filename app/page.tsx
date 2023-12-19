3ce204e4869ff1c832154511c2dffe747b40f8bd
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-19 19:41:41 +0000

    ping db

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,14 @@
-export default function Home() {
+import { getDatabase } from '@/utils/db';
+
+const fetchData = async () => {
+  const response = await getDatabase().then(() => {
+    console.log('done!');
+  });
+};
+
+export default async function Home() {
+  const homePage = await fetchData();
+
   return (
     <main>
       <h1 className="text-7xl">Hello world!!</h1>
