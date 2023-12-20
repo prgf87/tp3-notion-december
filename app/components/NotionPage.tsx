8b5c40586f54314137f192c40295b3cecc19a655
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-20 00:23:50 +0000

    categories mapped

--- app/components/NotionPage.tsx
+++ app/components/NotionPage.tsx
@@ -2,6 +2,31 @@
 import Link from 'next/link';
 import React from 'react';
 import { NotionRenderer } from 'react-notion-x';
+import dynamic from 'next/dynamic';
+
+const Code = dynamic(() =>
+  import('react-notion-x/build/third-party/code').then((m) => m.Code)
+);
+// const Collection = dynamic(() =>
+//   import('react-notion-x/build/third-party/collection').then(
+//     (m) => m.Collection
+//   )
+// );
+const Equation = dynamic(() =>
+  import('react-notion-x/build/third-party/equation').then((m) => m.Equation)
+);
+const Pdf = dynamic(
+  () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
+  {
+    ssr: false,
+  }
+);
+const Modal = dynamic(
+  () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal),
+  {
+    ssr: false,
+  }
+);
 
 import 'react-notion-x/src/styles.css';
 
@@ -13,7 +38,12 @@ export default function NotionPage({ recordMap }: any) {
       <div className="flex justify-center items-center pt-8">
         <Link href={'/'}>Home</Link>
       </div>
-      <NotionRenderer recordMap={recordMap} fullPage={true} darkMode={false} />
+      <NotionRenderer
+        recordMap={recordMap}
+        fullPage={true}
+        darkMode={false}
+        components={{ Code, Equation, Modal, Pdf }}
+      />
     </div>
   );
 }
