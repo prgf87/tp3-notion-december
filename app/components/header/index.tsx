1906fbef8e1778f9f5709932468364a9984cb20b
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-20 20:09:58 +0000

    adding links to header

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -8,16 +8,27 @@ type Props = {};
 export default function Header({}: Props) {
   return (
     <>
-      <div className="h-28 flex justify-center items-center pt-8 pb-4">
-        <Link href={'/'}>
-          <Image
-            src={logo}
-            alt="Website Logo"
-            width={125}
-            height={125}
-            className="h-[125px] max-w-[125px] transition-all duration-500 ease-linear hover:rounded-3xl hover:border hover:border-black/70"
-          />
-        </Link>
+      <div className="h-28 w-full grid grid-cols-3 pt-8 pb-4 mx-a">
+        <div>
+          <Link href={'/about'}>About</Link>
+          {/* <Link href={'/buy-coffee'}>Buy me a Coffee</Link> */}
+        </div>
+        <div>
+          <Link href={'/'}>
+            <Image
+              src={logo}
+              alt="Website Logo"
+              width={125}
+              height={125}
+              className="h-[125px] max-w-[125px] transition-all duration-500 ease-linear hover:rounded-3xl hover:border hover:border-black/70"
+            />
+          </Link>
+        </div>
+        <div>
+          <Link href={'/about'}>About</Link>
+
+          {/* <Link href={'/user'}>User Area</Link> */}
+        </div>
       </div>
       <h1 className="text-7xl font-extrabold  text-center pt-8 pb-4 uppercase drop-shadow-[-6px_4px_2px_rgba(0,0,0,0.55)]">
         Talking Points for Life
