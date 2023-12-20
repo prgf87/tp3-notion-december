4317ef8653731620ffaf56b5c7db77d0cc26f8cb
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-20 03:55:45 +0000

    rendering gifs

--- next.config.js
+++ next.config.js
@@ -5,6 +5,21 @@ const nextConfig = {
 
     return config;
   },
+  images: {
+    domains: [
+      // 'www.notion.so',
+      // 'notion.so',
+      'images.unsplash.com',
+      // 'pbs.twimg.com',
+      // 'abs.twimg.com',
+      // 's3.us-west-2.amazonaws.com',
+      // 'transitivebullsh.it',
+      'media.giphy.com',
+      'media3.giphy.com',
+      'media.tenor.com',
+      'www.sympathymessageideas.com',
+    ],
+  },
 };
 
 module.exports = nextConfig;
