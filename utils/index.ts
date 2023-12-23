209d2680bf87dcb66a6a3ede73654a5092fd7d31
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-23 12:31:12 +0000

    one function is not enough?

--- utils/index.ts
+++ utils/index.ts
@@ -1,13 +1,17 @@
 import { Client } from '@notionhq/client';
+import { useState } from 'react';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DB_ID as string;
 const categoriesId =
   '222e4acf63964c7e90c55228768a2db9?v=64300174648c41ee8a84e94f80881a64';
 
-export async function getDatabase(cursor: string | undefined = undefined) {
+export async function getDatabase() {
   //* Initial query made looking for the first complete pages/posts */
-  const pageSize = 12;
+  const pageSize: number = 12;
+  let cursor: string | undefined = undefined;
+  let results: any[] = [];
+  // let cursor: string | undefined = undefined;
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
@@ -23,11 +27,9 @@ export async function getDatabase(cursor: string | undefined = undefined) {
     page_size: pageSize,
     start_cursor: cursor,
   });
-  return response;
+  if (response.has_more) {
+    cursor = response.next_cursor ? response.next_cursor : undefined;
+  }
+  results = [...results, ...response.results];
+  return { ...response, response: { results: [results] } };
 }
-// export async function getCategories() {
-//   //* Initial query made looking for complete pages/posts */
-//   const response = await notion.pages.retrieve({ page_id: categoriesId });
-//   console.log(response);
-//   return response;
-// }
