dacea924fe342398406c844435ead50ec7c5e538
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-23 16:16:14 +0000

    cleaning up a bit

--- utils/index.ts
+++ utils/index.ts
@@ -3,10 +3,10 @@ import { Client } from '@notionhq/client';
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const databaseId = process.env.NOTION_DB_ID as string;
+const pageSize: number = 12;
 
-export async function getDatabase() {
+export async function fetchData() {
   //* GET query made looking for the first complete pages/posts */
-  const pageSize: number = 12;
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
@@ -25,8 +25,7 @@ export async function getDatabase() {
   return response;
 }
 
-export async function getMoreDatabase(cursor: string) {
-  const pageSize: number = 12;
+export async function fetchMoreData(cursor: string) {
   const response = await notion.databases.query({
     database_id: databaseId,
     filter: {
