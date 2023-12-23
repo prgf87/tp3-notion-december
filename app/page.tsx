d005bf5db1ec87f772a126d7e1b065a8c744308c
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-23 20:28:53 +0000

    created component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Loader from './components/Loader';
+import Categories from './components/categories';
 import Intro from './components/intro';
 
 export default async function Home() {
   return (
     <main className="gradient">
       <Intro />
+      <Categories />
       <Loader />
     </main>
   );
